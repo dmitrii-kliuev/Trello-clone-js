51474d7aab136725372f60f7fc285eb9447e7eab
Dmitrii Kliuev <dmitrii.kliuev.1989@gmail.com>
2021-05-13 01:56:23 +0300

    wip

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,31 +1,74 @@
 import '../index.html';
 import '../scss/main.scss';
-import {Task} from "./task";
+import {addTask, createTaskColumnElementHtml, Task, tasks} from "./task";
 
-const str: string = `
-[
-'in-box': ['task1','task2'],
-'todo': ['task3', 'task4'],
-'progress': ['task5', 'task6'],
-'review': ['task7', 'task8'],
-'done': ['task9', 'task10'], 
-]`;
+const mainWrapperElement = document.querySelector('.mainWrapper');
 
-interface Foo {
-    [key: string]: Task[];
+
+mainWrapperElement.addEventListener('click', mainWrapperElementClickHandler)
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAddNewTask = document.querySelector('.btnAddNewTask');
+
+    btnAddNewTask.addEventListener('click', () => {
+        const addNewTaskForm = document.getElementById('addNewTaskForm') as HTMLFormElement;
+        const columnField = document.getElementById('columnField') as HTMLInputElement;
+        const formData = new FormData(addNewTaskForm);
+
+        console.log(columnField.textContent);
+        console.log(formData.get('title'))
+        console.log(formData.get('description'))
+        console.log(formData.get('executor'))
+
+        addTask(columnField.textContent,
+            new Task({
+                title: formData.get('title') as string,
+                description: formData.get('description') as string,
+                executor: formData.get('executor') as string
+            }));
+
+        renderItemList();
+    })
+});
+
+
+function mainWrapperElementClickHandler(event: any) {
+    event.stopPropagation();
+
+    const {target} = event;
+    const {classList} = target;
+    const isAddTaskButton = classList.contains('addTask')
+
+    if (isAddTaskButton) {
+        const columnField = document.querySelector<HTMLInputElement>('#columnField');
+        columnField.textContent = target.parentNode.id;
+    }
 }
 
-let foo: Foo = {};
-foo['in-box'] = [];
-foo['in-box'].push(new Task({executor: 'Vasya', description: 'do do do', title: 'mega Do 1'}))
-foo['in-box'].push(new Task({executor: 'Mike', description: 'do do do', title: 'mega Do 2'}))
+function fillTasks() {
+    addTask('in-box', new Task({
+        title: 'Lorem ipsum dolor',
+        description: 'Lorem ipsum dolor sit amet',
+        executor: 'Bob Marks'
+    }));
 
-foo['todo'] = [];
-foo['todo'].push(new Task({executor: 'Dmitrii', description: 'do do do', title: 'mega Do 3'}))
-foo['todo'].push(new Task({executor: 'Petr', description: 'do do do', title: 'mega Do 4'}))
-
-console.log(foo);
-
-for (const task of foo['todo']) {
-    console.log(task.executor)
+    addTask('in-box', new Task({executor: 'Vasya', description: 'do do do', title: 'mega Do 1'}));
+    addTask('in-box', new Task({executor: 'Mike', description: 'do do do', title: 'mega Do 2'}));
+    addTask('todo', new Task({executor: 'Dmitrii', description: 'do do do', title: 'mega Do 3'}));
+    addTask('todo', new Task({executor: 'Petr', description: 'do do do', title: 'mega Do 4'}));
 }
+
+function renderItemList(): void {
+    mainWrapperElement.innerHTML =
+        Object
+            .keys(tasks)
+            .map(columnName => createTaskColumnElementHtml(columnName))
+            .join('');
+}
+
+function initApplication() {
+    fillTasks()
+    renderItemList();
+}
+
+initApplication();
--- src/ts/task.ts
+++ src/ts/task.ts
@@ -19,3 +19,49 @@ export class Task {
         this.id = Date.now() + (Math.floor(Math.random() * 1000001));
     }
 }
+
+export const tasks: ITasks = {};
+
+export function addTask(column: string, task: Task): void {
+    if (!tasks[column]) {
+        tasks[column] = [];
+    }
+
+    tasks[column].push(task);
+}
+
+export function createTaskColumnElementHtml(columnName: string): string {
+    const columnTasks = tasks[columnName];
+    const tasksHTML = columnTasks.map(t => createTaskElementHTML(t)).join('');
+
+    return `
+    <div class="column" id="${columnName}">
+        <div class="column__header">
+            <div class="taskCounter">${columnTasks.length}</div>
+            <div class="column__title">${columnName}</div>
+            <div class="column__menu">
+                <i class="fas fa-plus"></i>
+            </div>
+        </div>
+        <div class="taskList">${tasksHTML}</div>
+        <label for="modal_1" class="addTask">
+            <i class="fas fa-plus"></i>
+            Add another task...
+        </label>
+    </div>
+    `;
+}
+
+export function createTaskElementHTML(task: Task): string {
+    return `
+        <div class="task" data-id="${task.id}">
+            <div class="task__title">${task.title}</div>
+            <div class="task__description">${task.description}</div>
+            <div class="task__executor">${task.executor}</div>
+        </div>
+        `;
+}
+
+export interface ITasks {
+    [key: string]: Task[];
+}
